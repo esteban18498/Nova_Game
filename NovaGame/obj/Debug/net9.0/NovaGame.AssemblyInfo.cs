b09b9a441e48d532e4c16cd6bd09f8971422708a
Fianriel <esteban18498@gmail.com>
2025-04-03 12:28:57 -0300

    [test] base sdl2 implementation
    tested base sdl2 implementation with openGL and a blue canvas

--- NovaGame/obj/Debug/net9.0/NovaGame.AssemblyInfo.cs
+++ NovaGame/obj/Debug/net9.0/NovaGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NovaGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dc5277d5dc6e57a28f2276c335ec5d384c6cf75")]
 [assembly: System.Reflection.AssemblyProductAttribute("NovaGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NovaGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
